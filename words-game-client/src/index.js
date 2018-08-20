f77e697bf4128e6c2ee582f0b9df840871821f46
Brandon <btkim@uw.edu>
2018-08-20 01:58:09 -0700

    react tutorial #3 and we can finally use jsx. working on primitive UI mockup

--- words-game-client/src/index.js
+++ words-game-client/src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import GameClientWindow from './words_client.js';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<GameClientWindow />, document.getElementById('root'));
+
 registerServiceWorker();
--- words-game-client/src/words_client.js
+++ words-game-client/src/words_client.js
@@ -1,5 +1,9 @@
 'use strict';
 
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './words-game.css';
+
 const e = React.createElement;
 
 class GameClientWindow extends React.Component {
@@ -9,16 +13,40 @@ class GameClientWindow extends React.Component {
 
   render() {
     const chatBar = React.createElement(ChatBar);
-    return React.createElement('div', null, ['Hello', chatBar]);
+    // return React.createElement('div', null, ['Hello', chatBar]);
+    return <div class="gameClientWindow">
+      <PlayerList />
+    </div>;
   }
 }
 
 class ChatBar extends React.Component {
   render() {
-    return React.createElement('div', ['class', 'chatBar'], 'chat');
+    // return React.createElement('div', ['class', 'chatBar'], 'chat');
+    return <div class="chatBar">chat</div>;
   }
 }
 
-const root = document.getElementById('game_client_container');
-const element = React.createElement(GameClientWindow, null);
-ReactDOM.render(element, root);
+class PlayerList extends React.Component {
+  render() {
+    return <div class="playerListCard">
+      Players List
+      <ul className="playerList">
+        <PlayerListEntry number="1"/>
+        <PlayerListEntry number="2"/>
+      </ul>
+    </div>;
+  }
+}
+
+class PlayerListEntry extends React.Component {
+  render() {
+    return <li class="playerListEntry">entry {this.props.number}</li>;
+  }
+}
+//
+// const root = document.getElementById('game_client_container');
+// const element = React.createElement(GameClientWindow, null);
+// ReactDOM.render(element, root);
+
+export default GameClientWindow;
