bd95217fec02b792e66395ee180c2d1de6007a71
kimchi <kimchi@dev.grailwar.com>
2018-09-06 00:58:46 -0700

    read words file

--- library-gen/LibraryGen.java
+++ library-gen/LibraryGen.java
@@ -1,3 +1,39 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class LibraryGen {
-  
+
+  public static void main(String[] args) {
+
+    // open a library file from cmd line argument
+    // expect this to just be text, 1 word per line
+    if (args.length != 1) {
+      instructions();
+      return;
+    }
+
+    File wordsFile = new File(args[0]);
+    // if (!wordsFile.exists()) {
+    //   System.out.println("This file does not exist!");
+    //   return;
+    // }
+    try {
+      Scanner reader = new Scanner(wordsFile);
+
+      while (reader.hasNext()) {
+        System.out.println(reader.nextLine().trim().toLowerCase());
+      }
+    } catch (FileNotFoundException e) {
+      System.out.println("This file does not exist!");
+      return;
+    }
+
+  }
+
+  private static void instructions() {
+    System.out.println("Please give a single argument: \n" +
+      "java LibraryGen [path/to/wordlist]");
+  }
+
 }
