d0b370ef8760fbec9d45b4adb36b26c70f735f0a
kimchi <kimchi@dev.grailwar.com>
2018-09-06 01:20:22 -0700

    generate substrings from words

--- library-gen/LibraryGen.java
+++ library-gen/LibraryGen.java
@@ -1,6 +1,6 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
 
 public class LibraryGen {
 
@@ -14,20 +14,46 @@ public class LibraryGen {
     }
 
     File wordsFile = new File(args[0]);
-    // if (!wordsFile.exists()) {
-    //   System.out.println("This file does not exist!");
-    //   return;
-    // }
+
     try {
       Scanner reader = new Scanner(wordsFile);
 
+      // setup a map b/c we can probably hold this all in memory
+      // we'll just throw into this
+      Map<String, Set<String>> substringMap = new HashMap<String, Set<String>>();
+
       while (reader.hasNext()) {
-        System.out.println(reader.nextLine().trim().toLowerCase());
+        String word = reader.nextLine().trim().toLowerCase();
+        Set<String> substrings = genSubstrings(word);
+
+        System.out.println(word = ":");
+        System.out.println(substrings);
       }
     } catch (FileNotFoundException e) {
       System.out.println("This file does not exist!");
       return;
     }
+
+  }
+
+  /**
+  * Generate all 2 and 3 character substrings of the word
+  * Assumes the input is all lowercased
+  */
+  private static Set<String> genSubstrings (final String word) {
+    // Idk let's just do a sliding window thing.
+    Set<String> substrings = new TreeSet<String>();
+    for(int i = 0; i < word.length(); i++) {
+      if(i+2 <= word.length()) {
+        substrings.add(word.substring(i, i+2));
+      }
+
+      if(i+3 <= word.length()) {
+        substrings.add(word.substring(i, i+3));
+      }
+    }
+
+    return substrings;
 
   }
 
