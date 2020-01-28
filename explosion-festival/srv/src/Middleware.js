498a539863d433250a0354192664a3796d22f856
brandonk <brand0nk@users.noreply.github.com>
2020-01-27 22:36:47 -0800

    setting up server

---
+++ explosion-festival/srv/src/Middleware.js
@@ -0,0 +1,22 @@
+// @flow
+
+function getUserSession(req, res, next) {
+    var accessToken = req.body.accessToken;
+    if(accessToken != null) {
+        req.UID = -1;
+    } else {
+        req.UID = -1;
+    }
+    next();
+}
+
+// catch any users without a session and throw them to login
+function gateAuthed(req, res, next) {
+    if(req.UID != null) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+}
+
+module.exports = {getUserSession, gateAuthed};
