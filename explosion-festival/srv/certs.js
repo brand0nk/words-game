1e2d9f9862925f4378648649845ed49acf9d9b87
brandonk <brand0nk@users.noreply.github.com>
2020-01-28 01:07:35 -0800

    nvm certs aren't working i'm tired of this for now

--- explosion-festival/srv/certs.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const app = express();
-const port = 4567;
-
-app.use('/.well-known/acme-challenge', express.static('certs'));
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
